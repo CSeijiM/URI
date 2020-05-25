6f0b70853c97d1f8447d8ecbf2bc42df1d83b440
CSeijiM <cesarseiji95@gmail.com>
2020-05-25 00:38:12 -0300

    24/05/2020

--- iniciante/1047.c
+++ iniciante/1047.c
@@ -2,26 +2,54 @@
 
 int main()
 {
-    int inicio_h,inicio_m, fim_h, fim_m; 
-    double duracao_h, duracao_m;
+    int inicio_h,inicio_m, fim_h, fim_m, duracao_h, duracao_m;
     scanf("%d%d%d%d", &inicio_h,&inicio_m,&fim_h,&fim_m);
 
-    if(inicio_h == fim_h && inicio_m == fim_m)
+    if(inicio_h == fim_h)
     {
         duracao_h = 24;
-        duracao_m = 0;
+        if(inicio_m == fim_m){  
+            duracao_m = 0;
+        }
+        else if(inicio_m < fim_m){
+            duracao_m = fim_m - inicio_m;
+        }
+        else{
+            duracao_h = 23;
+            duracao_m = inicio_m - fim_m;
+        }
+        
     }
     else if(inicio_h > fim_h)
     {
         duracao_h = (24-inicio_h) + fim_h;
+        if(inicio_m == fim_m){  
+            duracao_m = 0;
+        }
+        else if(inicio_m < fim_m){
+            duracao_m = fim_m - inicio_m;
+        }
+        else{
+            duracao_h = (23-inicio_h) + fim_h;
+            duracao_m = inicio_m - fim_m;
+        }
     }
     else if(inicio_h < fim_h)
     {
 
-        duracao_h = (((fim_h*60)+(fim_m))-((inicio_h*60)+(inicio_h)))/60;
-        duracao_m = (((fim_h*60)+(fim_m))-((inicio_h*60)+(inicio_h)))%60;
+        duracao_h = fim_h - inicio_h;
+        if(inicio_m == fim_m){  
+            duracao_m = 0;
+        }
+        else if(inicio_m < fim_m){
+            duracao_m = fim_m - inicio_m;
+        }
+        else{
+            duracao_h = fim_h - inicio_h - 1;
+            duracao_m = 60 - (inicio_m - fim_m);
+        }
     }
 
-    printf("O JOGO DUROU %.0lf HORA(S) E %.0lf MINUTO(S)\n", duracao_h, duracao_m);
+    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", duracao_h, duracao_m);
     return(0);
 }
