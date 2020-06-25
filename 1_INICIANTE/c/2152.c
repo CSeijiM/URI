200be80c1620ac087bf8eb7c41ab3c16d6e0e862
CSeijiM <cesarseiji95@gmail.com>
2020-06-24 23:19:19 -0300

    24/06/2020

--- 1_INICIANTE/c/2152.c
+++ 1_INICIANTE/c/2152.c
@@ -1,9 +1,43 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 int main(){
     int c,
-        h,m,o;
+        h,m,o,
+        i;
+    char msg[100],aux[100];
 
+    scanf("%d",&c);
+    for(i=0;i<c;i++){
+        memset(msg,0,strlen(msg));
+        memset(aux,0,strlen(aux));
+        
+        scanf("%d %d %d",&h,&m,&o);
+        sprintf(aux, "%d", h);
+        if(h<10){
+            strcat(msg,"0");
+            strcat(msg,aux);
+        }
+        else strcat(msg,aux);
+        
+        strcat(msg,":");
+
+        sprintf(aux, "%d", m);
+        if(m<10){
+            strcat(msg,"0");
+            strcat(msg,aux);
+        }
+        else strcat(msg,aux);
+        
+        if(o==1){
+            printf("%s - A porta abriu!\n",msg);
+        }
+        else printf("%s - A porta fechou!\n",msg);
+
+        memset(msg,0,strlen(msg));
+        memset(aux,0,strlen(aux));
+    }
         
     return 0;
 }
