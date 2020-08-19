23f7328e1f5286dad36aeb1df1df0c0e97a663a4
CSeijiM <cesarseiji95@gmail.com>
2020-08-19 17:16:23 -0300

    19/08/2020

--- 1_INICIANTE/c/2540.c
+++ 1_INICIANTE/c/2540.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 
 int main(){
-
+    int n,i,v;
+    while(scanf("%d",&n)!=EOF){
+        int favo=0;
+        for(i=0;i<n;i++){
+            scanf("%d",&v);
+            if(v==1){
+                favo++;
+            }
+        }
+        if(favo>=(n*(2.0/3.0))){
+            printf("impeachment\n");
+        }
+        else printf("acusacao arquivada\n");
+    }
     return 0;
 }
--- 1_INICIANTE/c/2552.c
+++ 1_INICIANTE/c/2552.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 
-void leitura(int **matriz,int num_linha,int num_col){
-    int i,j;
-    for(i=0;i<num_linha;i++){
-        for(j=0;j<num_col;j++){
-            scanf("%d ",matriz[i][j]);
-        }
-    }
-}
-
 int main(){
-    int linha,coluna;
+    int i,j,linha,coluna;
 
     while(scanf("%d %d",&linha,&coluna)!=EOF){
-        int matriz[linha][coluna];
-        
+        int matriz[linha][coluna],matriz2[linha][coluna];
+
+        for(i=0;i<linha;i++){
+            for(j=0;j<coluna;j++){
+                scanf("%d",&matriz[i][j]);
+                matriz2[i][j]=matriz[i][j];
+            }
+        }
+        for(i=0;i<linha;i++){
+            for(j=0;j<coluna;j++){
+                if(matriz[i][j]==0){
+                    if(i>0){
+                        matriz2[i][j]+=matriz[i-1][j];
+                    }
+                    if(i<linha-1){
+                        matriz2[i][j]+=matriz[i+1][j];
+                    }
+                    if(j>0){
+                        matriz2[i][j]+=matriz[i][j-1];
+                    }
+                    if(j<coluna-1){
+                        matriz2[i][j]+=matriz[i][j+1];
+                    }
+                }
+            }
+        }
+        for(i=0;i<linha;i++){
+            for(j=0;j<coluna;j++){
+                if(matriz[i][j]==1){
+                    matriz2[i][j]=9;
+                }
+                printf("%d",matriz2[i][j]);
+            }
+            printf("\n");
+        }
     }
     return 0;
 }
